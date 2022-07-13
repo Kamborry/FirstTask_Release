d57ab57fff672897f5d8ae8088884b242cfbf380
Lugovaya Tatyana <norm.2013@yandex.ru>
2022-07-13 18:46:59 +0300

    Added all layout elements without RecyclerView.

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/main/MainPageFragment.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/main/MainPageFragment.kt
@@ -1,6 +1,7 @@
 package com.tlugovaya.firsttask_release.ui.main
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,7 +18,7 @@ class MainPageFragment : Fragment() {
 
     //private lateinit var viewModel: MainPageViewModel
     private var binding: FragmentMainPageBinding? = null
- 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,7 +41,7 @@ class MainPageFragment : Fragment() {
     }
 
     private fun showReleaseFragment(){
-        parentFragmentManager.beginTransaction()
+        childFragmentManager.beginTransaction()
                 .replace(R.id.container, ReleaseFragment.newInstance())
                 .commitNow()
     }
