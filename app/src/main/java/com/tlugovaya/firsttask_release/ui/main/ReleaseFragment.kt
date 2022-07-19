063d450058aabde30918c7e47498f8626eb888aa
Lugovaya Tatyana <norm.2013@yandex.ru>
2022-07-20 00:58:11 +0300

    Added new placeholder for titleImage, Edit collapsing toolbar. Теперь он полностью работоспособный, слава тебе ГОСПОДИИИИ

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/main/ReleaseFragment.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/main/ReleaseFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.tlugovaya.firsttask_release.R
 import com.tlugovaya.firsttask_release.databinding.FragmentReleaseBinding
 
 class ReleaseFragment : Fragment() {
@@ -29,6 +30,10 @@ class ReleaseFragment : Fragment() {
         with(binding) {
             //Заполняем данными binding.
             fillingWithDataFromMock(this, release)
+            toolbarImageRelease.downloadImage(
+                release.screenShorts.first(),
+                R.drawable.ic_title_placeholder
+            )
             release.posterUrl?.let { imageReleasePreview.downloadImage(it) }
             releasePoster.downloadImage(release.videoThumbnailUrl)
         }
@@ -37,6 +42,7 @@ class ReleaseFragment : Fragment() {
 
     private fun fillingWithDataFromMock(binding: FragmentReleaseBinding, release: Release) {
         with(binding) {
+            toolbar.title = release.title
             releaseAgeRating.text = release.ageRating
             genres.text = release.genres[0]
             releasePremiereDate.text = dateTimeFormatter(release.premiere)
