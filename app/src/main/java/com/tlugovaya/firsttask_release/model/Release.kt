e139f375c5857abb6239eee2e509513843884a51
Kamborry <norm.2013@yandex.ru>
2022-07-26 12:26:46 +0300

    filled in the data in the Release: titles and links to the poster.

--- app/src/main/java/com/tlugovaya/firsttask_release/model/Release.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/model/Release.kt
@@ -40,24 +40,24 @@ data class Repertory(
 fun getMockRepertory(): Repertory = Repertory(
     banners = listOf(
         Banner(
-            "https://xage.ru/media/uploads/2017/01/kong_skull_island_00.jpg",
-            null
+            imageUrl = "https://xage.ru/media/uploads/2017/01/kong_skull_island_00.jpg",
+            link = null
         ),
         Banner(
-            "https://xage.ru/media/uploads/2017/01/kong_skull_island.jpg",
-            null
+            imageUrl = "https://xage.ru/media/uploads/2017/01/kong_skull_island.jpg",
+            link = null
         ),
         Banner(
-            "https://geekcity.ru/wp-content/uploads/2021/03/Juy2rU86_9c.jpg",
-            null
+            imageUrl = "https://geekcity.ru/wp-content/uploads/2021/03/Juy2rU86_9c.jpg",
+            link = null
         ),
         Banner(
-            "https://horrorzone.ru/uploads/_pages2/70611/mini/glass-poster-14-750-300.jpg",
-            null
+            imageUrl = "https://horrorzone.ru/uploads/_pages2/70611/mini/glass-poster-14-750-300.jpg",
+            link = null
         ),
         Banner(
-            "https://www.film.ru/sites/default/files/images/11305437-977865.jpg",
-            null
+            imageUrl = "https://www.film.ru/sites/default/files/images/11305437-977865.jpg",
+            link = null
         )
 
         //Дополнительные ссылки, на случай, если используемые не будут работать.
@@ -77,38 +77,592 @@ fun getMockRepertory(): Repertory = Repertory(
         //https://thumbs.dfs.ivi.ru/storage26/contents/4/e/a222deddf3cb953989e957471fd0b8.jpg
     ),
     now = listOf(
-
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/0/8/6/8365680/741248381505.jpg
-        //https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/4/9/2/8356294/1d005924fddff94a8871d2102df5662c.jpg
-        //https://s1.afisha.ru/mediastorage/23/14/43ce20e54daa457f9f4d9de21423.jpg
-        //https://www.film.ru/sites/default/files/movies/posters/45780570-1122447.jpg
-        //https://kinobrest.by/uploads/events/56240pfp1.jpg
-
+        Release(
+            id = "",
+            title = "Темные очки",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/0/8/6/8365680/741248381505.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Главный герой",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/4/9/2/8356294/1d005924fddff94a8871d2102df5662c.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Юморист",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://s1.afisha.ru/mediastorage/23/14/43ce20e54daa457f9f4d9de21423.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Девятая",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://www.film.ru/sites/default/files/movies/posters/45780570-1122447.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Отчаянные дольщики",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://kinobrest.by/uploads/events/56240pfp1.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
     ),
     premiere = listOf(
-
-        //https://s2.afisha.ru/mediastorage/23/24/c3ab453460c8483e8ca7fdd12423.jpg
-        //https://s1.afisha.ru/mediastorage/62/4d/32a990e368334a07a59ca3654d62.jpg
-        //https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/3/0/8/8330803/2232b59f9bafb303d6f3bc2caa221d09.jpg
-        //https://thumbs.dfs.ivi.ru/storage3/contents/a/0/bdf24ef9f368cdf1e10dbad541131f.jpg
-        //https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/121a74ee-64eb-4ae4-80b7-f36a9f806e20/x1000
+        Release(
+            id = "",
+            title = "Мстители. Война бесконечности",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://s2.afisha.ru/mediastorage/23/24/c3ab453460c8483e8ca7fdd12423.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Доктор Стрэндж",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://s1.afisha.ru/mediastorage/62/4d/32a990e368334a07a59ca3654d62.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Агент 007. Не время умирать",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/3/0/8/8330803/2232b59f9bafb303d6f3bc2caa221d09.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Герой",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://thumbs.dfs.ivi.ru/storage3/contents/a/0/bdf24ef9f368cdf1e10dbad541131f.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Соник в кино",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/121a74ee-64eb-4ae4-80b7-f36a9f806e20/x1000",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
     ),
     kids = listOf(
-
-        //https://rossiakino.ru/upload/filmbase/posters/2563_mid_4727.jpg
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/9/1/2/8360219/3ff0c315f854ba76548438e7df446874.jpeg
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/0/2/5/8168520/959ee07b2de4328c6015a86bc4fc4ac9.jpg
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/8/8/4/8323488/48d7c8522d919bcce9c0cb690e6a5465.jpeg
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/6/7/2/8325276/bccf069da377fbed31f20d64d5b411f5.jpeg
-
+        Release(
+            id = "",
+            title = "Побег из космоса",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://rossiakino.ru/upload/filmbase/posters/2563_mid_4727.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Ледниковый период. Погоня за яйцами",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/9/1/2/8360219/3ff0c315f854ba76548438e7df446874.jpeg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Как приручить дракона 3",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/0/2/5/8168520/959ee07b2de4328c6015a86bc4fc4ac9.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Суперсемейка 2",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/8/8/4/8323488/48d7c8522d919bcce9c0cb690e6a5465.jpeg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Фиксики. Большой секрет",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/6/7/2/8325276/bccf069da377fbed31f20d64d5b411f5.jpeg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        )
     ),
     soon = listOf(
-
-        //https://s5.afisha.ru/mediastorage/a2/63/9abebeb141f7499f82fa5f4363a2.jpg
-        //https://timashevsk.ru/userfiles/images/preview_image/preview_afisha_cfmp9zk3af1582700432.jpg
-        //https://rb7.ru/system/movies/9323.jpg
-        //https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/9/4/9/8175949/ee89ae7cb1cc102e2067c96f5067fcf4.jpg
-        //https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/3/9/3/8356393/fc2d7830149b85b72d63ae05bf7808f5.jpg
+        Release(
+            id = "",
+            title = "Скажи ей",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://s5.afisha.ru/mediastorage/a2/63/9abebeb141f7499f82fa5f4363a2.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Яга",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://timashevsk.ru/userfiles/images/preview_image/preview_afisha_cfmp9zk3af1582700432.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Месть банши",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://rb7.ru/system/movies/9323.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Анчартед",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/9/4/9/8175949/ee89ae7cb1cc102e2067c96f5067fcf4.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
+        Release(
+            id = "",
+            title = "Веном 2",
+            ageRating = "",
+            genres = listOf(
+                "", ""
+            ),
+            premiere = "",
+            countries = listOf(
+                ""
+            ),
+            posterUrl = "https://static.kinoafisha.info/k/movie_posters/1080x1920/upload/movie_posters/3/9/3/8356393/fc2d7830149b85b72d63ae05bf7808f5.jpg",
+            videoThumbnailUrl = "",
+            duration = 0L,
+            directors = listOf(
+                ""
+            ),
+            cast = listOf(
+                "",
+                "",
+                ""
+            ),
+            story = null,
+            screenShorts = listOf(
+                "",
+                "",
+                ""
+            )
+        ),
     )
 )
 
