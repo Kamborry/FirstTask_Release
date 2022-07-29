1d338335df5a3bce552e4505a7387599fee3ff47
Kamborry <norm.2013@yandex.ru>
2022-07-29 15:27:25 +0300

    Added code into 3 Item Types Adapters. (3 methods and ViewHolders)

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/ItemBannerAdapter.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/ItemBannerAdapter.kt
@@ -4,34 +4,25 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tlugovaya.firsttask_release.databinding.ItemBannerBinding
 import com.tlugovaya.firsttask_release.model.Banner
-import com.tlugovaya.firsttask_release.model.Release
 
-class RepertoryAdapter : RecyclerView.Adapter<RepertoryAdapter.RepertoryViewHolder> {
+class ItemBannerAdapter : RecyclerView.Adapter<ItemBannerAdapter.ItemBannerViewHolder> {
 
     val banners: List<Banner> = emptyList()
-    val now: List<Release> = emptyList()
-    val premiere: List<Release> = emptyList()
-    val kids: List<Release> = emptyList()
-    val soon: List<Release> = emptyList()
-
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: RepertoryViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemBannerViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepertoryViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemBannerViewHolder {
         TODO("Not yet implemented")
     }
 
-    class RepertoryViewHolder(
-        //Может, нужно создать какой-то контейнер, в который будем складывать item?
+    class ItemBannerViewHolder(
         val bindingItemBanner: ItemBannerBinding
-        //val bindingItemTitle: ItemTitleBinding,
-        //val bindingItemRelease: ItemReleaseBinding
     ) : RecyclerView.ViewHolder(bindingItemBanner.root) {
     }
 }
--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/ItemReleaseAdapter.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/ItemReleaseAdapter.kt
@@ -1,4 +1,32 @@
 package com.tlugovaya.firsttask_release.ui.repertory
 
-class ItemReleaseAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tlugovaya.firsttask_release.databinding.ItemReleaseBinding
+import com.tlugovaya.firsttask_release.model.Release
+
+class ItemReleaseAdapter : RecyclerView.Adapter<ItemReleaseAdapter.ItemReleaseViewHolder> {
+
+    val now: List<Release> = emptyList()
+    val premiere: List<Release> = emptyList()
+    val kids: List<Release> = emptyList()
+    val soon: List<Release> = emptyList()
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemReleaseViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemReleaseViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    class ItemReleaseViewHolder(
+        val bindingItemRelease: ItemReleaseBinding
+    ) : RecyclerView.ViewHolder(bindingItemRelease.root) {
+    }
 }
