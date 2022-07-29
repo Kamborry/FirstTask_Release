16f8c7f4826bfab150ec9dedcc5f38256099f7f6
Kamborry <norm.2013@yandex.ru>
2022-07-29 18:22:25 +0300

    Added 2 func RecyclerView initialization.

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryFragment.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryFragment.kt
@@ -1,8 +1,14 @@
 package com.tlugovaya.firsttask_release.ui.repertory
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.tlugovaya.firsttask_release.databinding.FragmentRepertoryBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tlugovaya.firsttask_release.databinding.BannersListBinding
+import com.tlugovaya.firsttask_release.databinding.ReleaseListBinding
 
 class RepertoryFragment : Fragment() {
 
@@ -12,20 +18,45 @@ class RepertoryFragment : Fragment() {
 
     private lateinit var bannerAdapter: ItemBannerAdapter
     private lateinit var releaseAdapter: ItemReleaseAdapter
-    private lateinit val titleAdapter: ItemTitleAdapter
 
+    private lateinit var bindingRelease: ReleaseListBinding
+    private lateinit var bindingBanners: BannersListBinding
 
     private val viewModel: RepertoryViewModel by viewModels()
 
-    private val binding: FragmentRepertoryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bindingRelease = ReleaseListBinding.inflate(inflater, container, false)
 
-    private fun getData(){
-
-        viewModel.getRepertory().observe(viewLifecycleOwner){
-
-
+        viewModel.getRepertory().observe(viewLifecycleOwner) {
+            //bannerAdapter.submitList(it)
+            //releaseAdapter.submitList(it)
         }
 
+        initReleaseRecyclerView()
+        initBannerRecyclerView()
+
+        return bindingRelease.root
     }
 
+    private fun initReleaseRecyclerView() {
+        releaseAdapter = ItemReleaseAdapter()
+        val recyclerView = bindingRelease.listRelease
+        recyclerView.apply {
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = releaseAdapter
+        }
+    }
+
+    private fun initBannerRecyclerView() {
+       bannerAdapter = ItemBannerAdapter()
+        val recyclerView = bindingBanners.listBanners
+        recyclerView.apply {
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = bannerAdapter
+        }
+    }
 }
