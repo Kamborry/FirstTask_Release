17e24c8c647d4bb7427ea11c5897a4500ca92b47
Kamborry <norm.2013@yandex.ru>
2022-07-29 16:58:33 +0300

    Added adapter observer in fragment and viewModel repertory. Added titles in string recourse.

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryFragment.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryFragment.kt
@@ -2,6 +2,7 @@ package com.tlugovaya.firsttask_release.ui.repertory
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.tlugovaya.firsttask_release.databinding.FragmentRepertoryBinding
 
 class RepertoryFragment : Fragment() {
 
@@ -9,6 +10,22 @@ class RepertoryFragment : Fragment() {
         fun newInstance() = RepertoryFragment()
     }
 
+    private lateinit var bannerAdapter: ItemBannerAdapter
+    private lateinit var releaseAdapter: ItemReleaseAdapter
+    private lateinit val titleAdapter: ItemTitleAdapter
+
+
     private val viewModel: RepertoryViewModel by viewModels()
+
+    private val binding: FragmentRepertoryBinding
+
+    private fun getData(){
+
+        viewModel.getRepertory().observe(viewLifecycleOwner){
+
+
+        }
+
+    }
 
 }
--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryViewModel.kt
+++ app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RepertoryViewModel.kt
@@ -1,9 +1,16 @@
 package com.tlugovaya.firsttask_release.ui.repertory
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tlugovaya.firsttask_release.model.Repertory
 import com.tlugovaya.firsttask_release.model.getMockRepertory
 
 class RepertoryViewModel : ViewModel() {
+
+    private val repertoryLiveData: MutableLiveData<Repertory> = MutableLiveData<Repertory>()
+
+    fun getRepertory(): LiveData<Repertory> = repertoryLiveData
 
     val repertory = getMockRepertory()
 }
