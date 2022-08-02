f4069e5c4c1b46985e2f8f6c8c2afaa1b7b30ff7
Kamborry <norm.2013@yandex.ru>
2022-08-02 17:21:46 +0300

    Added files for Repertory RecyclerView.

--- app/src/main/java/com/tlugovaya/firsttask_release/ui/repertory/RecyclerViewItemType.kt
+++
@@ -1,8 +0,0 @@
-package com.tlugovaya.firsttask_release.ui.repertory
-
-sealed class RecyclerViewItemType {
-}
-
-class BannerItemType : RecyclerViewItemType()
-class TitleItemType : RecyclerViewItemType()
-class ReleaseItemType : RecyclerViewItemType()
